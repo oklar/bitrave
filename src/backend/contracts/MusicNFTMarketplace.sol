53add04342fb110494250eafe51a932cc677ea89
oklar <notanemail@noemailatallthisisfake.com>
2022-04-18 19:24:27 +0200

    added buying of nft functionality with tests

--- src/backend/contracts/MusicNFTMarketplace.sol
+++ src/backend/contracts/MusicNFTMarketplace.sol
@@ -19,6 +19,13 @@ contract MusicNFTMarketplace is ERC721("BitraveFi", "BRF"), Ownable {
 
     MarketItem[] public marketItems;
 
+    event MarketItemBought(
+        uint256 indexed tokenId,
+        address indexed seller,
+        address buyer,
+        uint256 price
+    );
+
     constructor(
         uint256 _royaltyFee,
         address _artist,
@@ -40,5 +47,21 @@ contract MusicNFTMarketplace is ERC721("BitraveFi", "BRF"), Ownable {
 
     function updateRoyaltyFee(uint256 _royaltyFee) external onlyOwner {
         royaltyFee = _royaltyFee;
+    }
+
+    function buyToken(uint256 _tokenId) external payable {
+        uint256 price = marketItems[_tokenId].price;
+        address seller = marketItems[_tokenId].seller;
+
+        require(
+            msg.value == price,
+            "Please send the asking price in order to complete the purchase"
+        );
+
+        marketItems[_tokenId].seller = payable(address(0));
+        _transfer(address(this), msg.sender, _tokenId);
+        payable(artist).transfer(royaltyFee);
+        payable(seller).transfer(msg.value);
+        emit MarketItemBought(_tokenId, seller, msg.sender, price);
     }
 }
