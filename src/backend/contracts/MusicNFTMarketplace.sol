beb77730ec41c1c35aac0cf918cfae3f82382d9b
oklar <notanemail@noemailatallthisisfake.com>
2022-04-18 18:07:38 +0200

    added constructor that mints new marketItems

--- src/backend/contracts/MusicNFTMarketplace.sol
+++ src/backend/contracts/MusicNFTMarketplace.sol
@@ -7,4 +7,34 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract MusicNFTMarketplace is ERC721("BitraveFi", "BRF"), Ownable {
     string public baseURI =
         "https://bafybeifdcym5lijteu7m3yndu273jz4yhlcey3lxcypaz7fjejjs33fqbq.ipfs.nftstorage.link"; // location for the metadata of the music nfts
+    string public baseExtension = ".json";
+    address public artist;
+    uint256 public royaltyFee;
+
+    struct MarketItem {
+        uint256 tokenId;
+        address payable seller;
+        uint256 price;
+    }
+
+    MarketItem[] public marketItems;
+
+    constructor(
+        uint256 _royaltyFee,
+        address _artist,
+        uint256[] memory _prices
+    ) payable {
+        require(
+            _prices.length * royaltyFee <= msg.value,
+            "Deployer must pay royalty fee for each token listed on the marketplace"
+        );
+
+        royaltyFee = _royaltyFee;
+        artist = _artist;
+        for (uint8 i = 0; i < _prices.length; i++) {
+            require(_prices[i] > 0, "Price must be greater than 0");
+            _mint(address(this), i);
+            marketItems.push(MarketItem(i, payable(msg.sender), _prices[i]));
+        }
+    }
 }
